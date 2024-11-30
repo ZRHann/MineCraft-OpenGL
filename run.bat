239252399e1b11c79ef79f0d32dd18d5c510bca7
ZRHan <zhengrh8@mail2.sysu.edu.cn>
2024-11-30 23:52:13 +0800

    删除.vscode; 添加依赖库

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 g++ -o opengl_hello_world.exe main.cpp ^
--I"D:/softwares/glfw-3.4.bin.WIN64/glfw-3.4.bin.WIN64/include" ^
--L"D:/softwares/glfw-3.4.bin.WIN64/glfw-3.4.bin.WIN64/lib-mingw-w64" ^
--L"D:\softwares\freeglut-MinGW-3.0.0-1.mp\freeglut\lib\x64" ^
--I"D:\softwares\freeglut-MinGW-3.0.0-1.mp\freeglut\include" ^
+-I"./requirements/glfw-3.4.bin.WIN64/glfw-3.4.bin.WIN64/include" ^
+-L"./requirements/glfw-3.4.bin.WIN64/glfw-3.4.bin.WIN64/lib-mingw-w64" ^
+-L"./requirements/freeglut-MinGW-3.0.0-1.mp/freeglut/lib/x64" ^
+-I"./requirements/freeglut-MinGW-3.0.0-1.mp/freeglut/include" ^
 -lglfw3 -lopengl32 -lgdi32 -lglu32
 .\opengl_hello_world.exe
 
